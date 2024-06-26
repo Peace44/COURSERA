e32b8b27c1b47a093b8a31a9cc6cb80dcfb37efe
Iragukujije Peace Bonerabose <peacebonerabose@gmail.com>
2024-06-26 21:46:56 +0200

    Update MaxPairwiseProd.cpp

--- UCSD/Data-Structures-and-Algorithms-Specialization/Course1-AlgorithmicToolbox/Assignment1/MaximumPairwiseProduct/MaxPairwiseProd.cpp
+++ UCSD/Data-Structures-and-Algorithms-Specialization/Course1-AlgorithmicToolbox/Assignment1/MaximumPairwiseProduct/MaxPairwiseProd.cpp
@@ -25,9 +25,11 @@ long long MaxPairwiseProduct(const std::vector<int>& numbers)
 int main()
 {
     int n;
+    
     std::cin >> n;
 
     std::vector<int> numbers(n);
+    
     for (int i = 0; i < n; i++) std::cin >> numbers[i];
     
     std::cout << MaxPairwiseProduct(numbers) << std::endl;
