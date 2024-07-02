25684426fa03631021d7f976eff2dd3b8b16b977
Peace44 <peacebonerabose@gmail.com>
2024-07-02 22:03:38 +0200

    ...

--- UCSD/Data-Structures-and-Algorithms-Specialization/Course1-AlgorithmicToolbox/Assignment2/IP1-Josephus/Josephus.py
+++ UCSD/Data-Structures-and-Algorithms-Specialization/Course1-AlgorithmicToolbox/Assignment2/IP1-Josephus/Josephus.py
@@ -1,40 +1,27 @@
-class JosephusList:
-    def __init__(self, rebels):
-        self.rebels = rebels
-        self.next_eliminator = rebels[0] 
-    
-    def __len__(self):
-        return len(self.rebels)
-
-    def __iter__(self):
-        pass
-
-    def __getitem__(self, id):
-        if id < 0 or id >= len(self):
-            raise IndexError("Index out of range")
-        return self.rebels[(self.next_eliminator + id) % len(self)]
-
-    def __repr__(self):
-        return str(self.rebels)
-    
-    def eliminate(self, k):
-        next_eliminator_index = self.rebels.index(self.next_eliminator)
-        next_rebel_to_eliminate_index = (next_eliminator_index + k - 1) % len(self)
-        self.rebels.pop(next_rebel_to_eliminate_index)
-        self.next_eliminator = self.rebels[(self.rebels.index(self.next_eliminator) + 1) % len(self)]
-    
-    def get_survivor(self, k):
-        while len(self) > 1:
-            self.eliminate(k)
-        return self.next_eliminator
-
-
-
 def greedy_josephus(n, k):
-    rebels = JosephusList([i for i in range(n)])
-    return rebels.get_survivor(k)
+    rebels = [i for i in range(n)]
+    nber_of_remaining_rebels = n
+    i = j = 0
+    while nber_of_remaining_rebels != 1:
+        if rebels[i] >= 0:
+            j = (j + 1) % k
+            if j == 0:
+                rebels[i] = -1  # killing rebel rebels[i]
+                nber_of_remaining_rebels -= 1
+        i = (i + 1) % n
+    return next(survivor for survivor in rebels if survivor >= 0)
+
+
+
+def recursive_josephus(n, k):
+    if n == 1:
+        return 0
+    else:
+        return (recursive_josephus(n-1, k) + k) % n
+
 
 
 if __name__ == "__main__":
     n, k = map(int, input().split())
     print(greedy_josephus(n, k))
+    print(recursive_josephus(n, k))
